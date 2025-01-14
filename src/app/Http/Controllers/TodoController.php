56835369c8cf393665ed5754fa9b310cc586cba7
wakuw <sldkfjsldfk@gmail.com>
2025-01-14 09:51:13 +0900

    refactor:コントローラのリファクタ

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -9,9 +9,16 @@ use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index()
     {
-        $todo = new Todo();
+        $todo = $this->todo->all();
         $todos = $todo->all();
 
         return view('todo.index', ['okabe' => $todos]); //②第一引数をtest.indexにするには？③変数名「okabe」で一覧表示
@@ -24,22 +31,17 @@ class TodoController extends Controller
 
     public function store(Request $request) //①代入されている値＋データ型調べる→object型ならnamespaceとクラス名
     {
-        dd($request);
         $inputs = $request->all();
 
-        $todo = new Todo();
-        $todo->user_id = Auth::id();
-        $todo->fill($inputs);
-        $todo->save();
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
-
+        $todo = $this->todo->find($id);
         return view('todo.show', ['todo' => $todo]);
     }
 }
